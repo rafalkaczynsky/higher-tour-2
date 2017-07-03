2c31fdc516ba5f6f086da8479a59392443c166bd
rafalkaczynsky <rafalkaczynsky1985@gmail.com>
2017-07-03 15:37:22 +0100

    Add few Icons to resources

--- src/styles/resources/icons.js
+++ src/styles/resources/icons.js
@@ -1,4 +1,18 @@
 export default {
-    "bible": require('./icons/chrome_reader_mode_black_96x96.png'),
-    "settings": require('./icons/settings_black_96x96.png'),
+    "bible": require('./icons/bible.png'),
+    "settings": require('./icons/settings.png'),
+    "events": require('./icons/events.png'),
+    "logo": require('./icons/logo.png'),
+    "facebook": require('./icons/facebook.png'),
+    "google": require('./icons/gmail.png'),
+    "twitter": require('./icons/twitter.png'),  
+
+
+    "bibleActive": require('./icons/bibleActive.png'),
+    "settingsActive": require('./icons/settingsActive.png'),
+    "eventsActive": require('./icons/eventsActive.png'),
+    "logoActive": require('./icons/logoActive.png'),
+    "facebookActive": require('./icons/facebookActive.png'),
+    "googleActive": require('./icons/gmailActive.png'),
+    "twitterActive": require('./icons/twitterActive.png'), 
 }
--- src/windows/sign-in.js
+++ src/windows/sign-in.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {ScrollView, View, Text} from 'react-native'
 
 import StyleSheet from '../styles'
 
@@ -10,15 +10,61 @@ export default class SignIn extends Component {
   render() {
     return (
       <View style={StyleSheet.window.default}>
-        <TextBox />
-        <Icon name="bible" />
+        <ScrollView style={{flex: 1, width: '100%'}}>
+        <View style={{alignItems: "center"}}>
+          <TextBox />
+          <View style={{flex:1 , alignItems: 'center', flexDirection: 'row', justifyContent: 'center'}}>
+          <Icon 
+            name="bible" 
+            style={{width: 50, height: 50, margin: 20}}
+          />
+          <Icon 
+            name="logo" 
+            style={{width: 50, height: 50, margin: 20}}
+          />
+          <Icon 
+            name="events" 
+            style={{width: 50, height: 50, margin: 20}}
+          />
+          <Icon 
+            name="settings" 
+            style={{width: 50, height: 50, margin: 20}}
+          />
+        </View>
+        <View style={{flex:1 , alignItems: 'center', flexDirection: 'row', justifyContent: 'center'}}>
+          <Icon 
+            name="bible" 
+            active
+            style={{width: 50, height: 50, margin: 20}}
+          />
+          <Icon 
+            name="logo"
+            active
+            style={{width: 50, height: 50, margin: 20}}
+        />
+          <Icon 
+            name="events"
+            active 
+            style={{width: 50, height: 50, margin: 20}}
+          />
+          <Icon 
+            name="settings"
+            active 
+            style={{width: 50, height: 50, margin: 20}}
+          />
+        </View>
+
         <Title 
           text="This is title component"  
           bgColor="brown" 
-          textColor="white"/>
+          textColor="white"
+          style={{marginBottom: 30}}  
+        />
+
         <Button 
           type="default"
           text="Button without icon"/>
+
         <Button 
           type="default"
           text="Button without icon"
@@ -36,25 +82,25 @@ export default class SignIn extends Component {
         <Button 
           type="facebook"
           text="Sign in with Facebook"
-          iconName="bible"
+          iconName="facebook"
           buttonStyle={{margin: 10, width: '70%'}}
         />
 
         <Button 
           type="twitter"
           text="Sign in with Twitter"
-          iconName="bible"
+          iconName="twitter"
           buttonStyle={{margin: 10, width: '70%'}}
         />
-
 
          <Button 
           type="google"
           text="Sign in with Google"
-          iconName="bible"
+          iconName="google"
           buttonStyle={{margin: 10, width: '70%'}}
         />          
-
+        </View>
+        </ScrollView>
       </View>
     )
   }
