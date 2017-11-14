612a06e8d6615910427bdac2aa8080b3e03de2eb
Rafal Kaczynsky <rafal@Maxims-Mac-mini.lan>
2017-11-14 15:56:35 +0000

    Question type basic structure

--- src/windows/questions.js
+++ src/windows/questions.js
@@ -6,18 +6,9 @@ import {colors} from '../styles/resources'
 
 import {TextBox, Icon, Title, Button, TabMenu, Header, ListItem} from '../components'
 
-export default class Questions extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const {onGoBack, onHome, onSettings, onBible, session, questionIndex, handleAnswer} = this.props
-
-    const questionText = session.Questions[questionIndex+1].Question
-    const Answers =  session.Questions[questionIndex+1].Answers
-
-    console.log(Answers) 
+class QuestionText extends Component {
+  render(){
+    const {onGoBack, Answers, questionText, onHome, onSettings, onBible, session, questionIndex, handleAnswer} = this.props
 
     return (
       <View style={StyleSheet.window.default}>
@@ -59,5 +50,102 @@ export default class Questions extends Component {
         />
       </View>
     )
+  }
+}
+
+class QuestionImageSlider extends Component {
+  render() {
+    const  {onGoBack, onHome, onBible, onSettings} = this.props
+    return(
+      <View style={StyleSheet.window.default}>
+      {questionIndex === 0 &&
+        <Header
+          text='Question'
+          onBack
+          onBackCallback={onGoBack}
+        />}
+        <ScrollView style={{width: '100%'}}>
+          <View style={StyleSheet.settings.container}>
+            <Text>QUESTION SLIDER NOT FULL SCREEN</Text>
+          </View>
+        </ScrollView>
+        <TabMenu
+            onHome={onHome}
+            onBible={onBible}
+            onSettings={onSettings}
+        />
+      </View>
+    )
+  }
+}
+
+class QuestionImageSliderFullScreen extends Component {
+  render() {
+    return(
+      <View style={StyleSheet.window.default}>
+        {/* full screen image slider */}
+      </View>
+    )
+  }
+}
+
+class QuestionImageSurvey extends Component {
+  render() {
+    const  {onGoBack, onHome, onBible, onSettings} = this.props
+
+    return(
+      <View style={StyleSheet.window.default}>
+      {questionIndex === 0 &&
+        <Header
+          text='Question'
+          onBack
+          onBackCallback={onGoBack}
+        />}
+        <ScrollView style={{width: '100%'}}>
+          <View style={StyleSheet.settings.container}>
+            <Text>QUESTION SURVAY NOT FULL SCREEN</Text>
+            {/*buttons agree / disagree*/}
+          </View>
+        </ScrollView>
+        <TabMenu
+            onHome={onHome}
+            onBible={onBible}
+            onSettings={onSettings}
+        />
+      </View>
+    )
+  }
+}
+
+class QuestionImageSurvey -FullScreen extends Component {
+  render() {
+    return(
+      <View style={StyleSheet.window.default}>
+        {/* full screen image slider */}
+        {/*buttons agree / disagree*/}
+      </View>
+    )
+  }
+}
+
+export default class Questions extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const {onGoBack, onHome, onSettings, onBible, session, questionIndex, handleAnswer} = this.props
+
+    const questionText = session.Questions[questionIndex+1].Question
+    const Answers =  session.Questions[questionIndex+1].Answers
+
+    const questionType = session.questionType 
+    console.log(session)
+
+          if (questionType === 'text')        return <QuestionText Answers={Answers} questionText={questionText} onGoBack={onGoBack} onHome={onHome}  onSettings={onSettings} onBible={onBible} session={session} questionIndex={questionIndex} handleAnswer={handleAnswer}/>
+     else if (questionType === 'imageSlider') return <QuestionImageSlider onGoBack={onGoBack} onHome={onHome} onBible={onBible} onSettings={onSettings}  />
+     else if (questionType === 'imageSurvey') return <QuestionImageSurvey  onGoBack={onGoBack} onHome={onHome} onBible={onBible} onSettings={onSettings} />
+     else return <Text>Warning! Something wrong - no questionType found</Text>
+
   }
 }
